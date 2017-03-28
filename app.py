88f7b38ef1f73575cce7ef01e227dcba2fe51282
Will Czubiak <willczubiak@gmail.com>
2017-03-28 04:12:13 +0000

    changed file name, and added in flask/socketio to render webpage

---
+++ app.py
@@ -0,0 +1,16 @@
+import os, flask, flask_socketio
+
+app = flask.Flask(__name__)
+socketio = flask_socketio.SocketIO(app)
+
+
+@app.route('/')
+def hello():
+    return flask.render_template('index.html')
+
+socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
