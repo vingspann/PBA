79142113784a5875d9600b098d2008400607b384
jayrfitz <jfitzgerald@csumb.edu>
2017-03-28 03:52:56 +0000

    pokemon with health

---
+++ scripts/battlePage/yoPokemon.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import '../src/App.css';
+
+
+export class Content extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                <h1>pokemon</h1>
+                <div>
+                     'Pikachu'
+                     '90/100'
+                     'Charizard'
+                     '80/100'
+                     'blastoise'
+                     '80/100'
+                </div>
+            </div>
+        );
+    }
+}
