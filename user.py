5bf6d0a7707a62fc48c83f338a4a7f9b1ff003df
Will Czubiak <willczubiak@gmail.com>
2017-04-20 02:58:33 +0000

    added in constructor to file

--- user.py
+++ user.py
@@ -3,25 +3,26 @@ from character import character
 
 class user():
     
-    name = ''
-    pokemon = []
-    hasWon = False
-    currentPokemon = 0
-    recentMove = 1
-    ID = None
-    for i in range(2):
-        pokemon.append(character())
+    def __init__(self):
+        self.name = ''
+        self.pokemon = [character(), character()]
+        self.hasWon = False
+        self.currentPokemon = 0
+        self.recentMove = 0
+        self.ID = None
+    #for i in range(2):
+     #   pokemon.append(character())
         
     # This updates the number of the most recent move
     def updateRecent(self, move):
-        recentMove = move
-        
-    # This updates the number of the current pokemon
-    def updateCurrent(self, poke):
-        currentMonster = poke
+        self.recentMove = move
         
     # Sets the persons name for chat
     def nameSet(self, name):
-        self.name = name;
+        self.name = name
         
-    
+    def getCurrentPokemon():
+        return self.currentPokemon
+        
+    def __repr__(self):
+        return "Player" + str(self.pokemon)
