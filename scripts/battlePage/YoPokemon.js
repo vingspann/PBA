0138f6cfe644a2d42ca0db8a69cab45baf9976a4
jayrfitz <jfitzgerald@csumb.edu>
2017-04-20 01:05:24 +0000

    added 2 socket.on to yopokemon

--- scripts/battlePage/YoPokemon.js
+++ scripts/battlePage/YoPokemon.js
@@ -7,12 +7,32 @@ export class YoPokemon extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            'pokemon': [{ 'character' : 'Pikachu', 'health' : '0.90', 'spriteLink' : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'}
-                       ,{ 'character' : 'Charazard', 'health' : '0.80', 'spriteLink' : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'}]
+            'pokemon' : []
+            // 'pokemon': [{ 'character' : 'Pikachu', 'health' : '0.90', 'spriteLink' : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'}
+            //           ,{ 'character' : 'Charazard', 'health' : '0.80', 'spriteLink' : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'}]
         };
     }
     componentDidMount(){
+        // Allows moves to be dynamically updated.
+        Socket.on('updatePokemon', (data) =>{
+            this.setState({
+                'name'  : data['name'],
+                'link'  : data['link'],
+                'maxHealth': data['maxHealth'],
+                'curHealth' : data['curHealth']
+                
+            })
+        })
         
+        // Allows moves to be dynamically updated.
+        Socket.on('updateOpPokemon', (data) =>{
+            this.setState({
+                'name'  : data['name'],
+                'link'  : data['link'],
+                'health': data['health']
+                
+            })
+        })
     }
     render() {
         let pokemon = this.state.pokemon.map((n, index) =>
