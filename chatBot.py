5fc9c7b6fb793616591a8a825defc1b7a2238243
Will Czubiak <willczubiak@gmail.com>
2017-04-24 02:53:19 +0000

    added in type command and gender

--- chatBot.py
+++ chatBot.py
@@ -8,8 +8,12 @@ class bot():
         if msg[0] == "!!":
             if (msg[1] == "help"):
                 return "Please consult your pokedex"
-            if (msg[1] == "bike"):
+            elif (msg[1] == "bike"):
                 return "There's a time and place for everything, but not now."
+            elif (msg[1] == "gender"):
+                return "Your gender is your own, I won't ask if you're a boy or a girl!"
+            elif (msg[1] == "type"):
+                return "1337"
         return ''
     
     def check(self, message):
