6ca7dc20f7589fcb10d6fc410a1681af14f70cf3
Brandon <bavery@csumb.edu>
2017-05-02 23:47:54 +0000

    adding styling and content for the landing page

--- scripts/battlePage/Content.js
+++ scripts/battlePage/Content.js
@@ -89,24 +89,38 @@ export class Content extends React.Component{
                     <div>
                         <Carousel>
                             <Carousel.Item>
-                                <img width={900} height={500} alt="900x500" src="../static/img/Pokémon_Stadium_2.jpg"/>
+                                <img width={900} height={500} className="carouselImg" alt="900x500" src="../static/img/Pokémon_Stadium_2.jpg"/>
                                 <Carousel.Caption>
-                                    <h3>First slide label</h3>
-                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                    <h3>Welcome to PBA Stadium!!</h3>
+                                    <p>We built this site to simulate small, simple pokemon battles.</p>
                                 </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <img width={900} height={500} alt="900x500" src="../static/img/pokemonStadium.jpg"/>
+                                <img width={900} height={500} className="carouselImg" alt="900x500" src="../static/img/pokemonStadium.jpg"/>
                                 <Carousel.Caption>
-                                    <h3>Second slide label</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <h3>Battle Log</h3>
+                                    <p>The Battle Log is the black window in the center of your screen.  All moves and results will be displayed here.</p>
                                 </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <img width={900} height={500} alt="900x500" src="../static/img/PBALogo.png"/>
+                                <img width={900} height={500} className="carouselImg" alt="900x500" src="../static/img/Pokémon_Stadium_2.jpg"/>
                                 <Carousel.Caption>
-                                    <h3>Third slide label</h3>
-                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                    <h3>Pokemon Info</h3>
+                                    <p>To the left of the Battle Log is the Pokemon Info window.  This window shows your currently selected pokemon and the opponents currently selected pokemon.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img width={900} height={500} className="carouselImg" alt="900x500" src="../static/img/Pokémon_Stadium_2.jpg"/>
+                                <Carousel.Caption>
+                                    <h3>Pokemon Commands</h3>
+                                    <p>Once you have joined a battle you will see a few buttons below the Battle Log where you can select the move you want your pokemon to use.  By double clicking the button, the move will be set and clicking Confirm Move will let the system know you are ready to continue.  You can also switch out the pokemon you are using or surrender the battle if you feel so inclined.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img width={900} height={500} className="carouselImg" alt="900x500" src="../static/img/Pokémon_Stadium_2.jpg"/>
+                                <Carousel.Caption>
+                                    <h3>Chat</h3>
+                                    <p>To the right of the Battle Log is our chat window.  Feel free to converse about Pokemon or talk trash.</p>
                                 </Carousel.Caption>
                             </Carousel.Item>
                         </Carousel>
